6fc398b98bc7d312395536aabf3bb5cdd1a37e28
OlikaDan <oliya.daniel@gmail.com>
2021-04-12 00:04:27 +0300

    First commit

---
+++ src/Monster.java
@@ -0,0 +1,16 @@
+public abstract class Monster extends Entity implements Fighter {
+
+    // поля суперкласса класса
+    private int force; //сила повреждения
+
+    // конструктор
+    public Monster(String name, int force) {
+        super(name);
+        this.force = force;
+    }
+
+    //метод возвращает силу повреждения
+    protected int getForce(){
+        return force;
+    }
+}
